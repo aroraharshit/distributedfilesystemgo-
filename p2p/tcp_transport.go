33e276a271cfd69c08a39a01f68525bf8f0f1035
Harshit Arora <harora33.ha@gmail.com>
2025-04-12 16:30:54 +0530

    Enforce LF endings in Go files

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,80 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// TCPPeer represent the remote node over a TCP established connection.
+type TCPPeer struct {
+	//conn is the underlying connection of the peer
+	conn net.Conn
+	//if we dial and retrieve c conn => outbound == true
+	//if we accept and retrieve a conn => outbound false
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+type TCPTransport struct {
+	listenAddress  string
+	listner        net.Listener
+	shakeHands HandShakeFunc
+	decoder Decoder
+	mu             sync.RWMutex
+	peers          map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		shakeHands: NopHandShakeFunc,
+		listenAddress:  listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+
+	var err error
+
+	t.listner, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+	go t.startAcceptLoop()
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listner.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+		}
+		fmt.Printf("New incoming connection %+v\n",conn)
+		go t.handleConn(conn)
+	}
+}
+
+type Temp struct{}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+	if err:=t.shakeHands(peer);err!=nil{
+		
+	}
+	//Read Loop
+	msg:=&Temp{}
+	for {
+		if err:=t.decoder.Decode(conn,msg);err!=nil{
+			fmt.Printf("TCP error %s\n",err)
+			continue
+		}
+	}
+
+}
